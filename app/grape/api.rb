158a9ea0e22ab9f7d461cd7bfe00930c0bdeb608
ivan <peij@163.com>
2014-05-13 18:28:27 +0800

    增加日志输出

--- app/grape/api.rb
+++ app/grape/api.rb
@@ -6,7 +6,14 @@ module GjbApi
     before do
       my_logger ||= Logger.new("#{Rails.root}/log/my.log")
       # my_logger.info env
-      my_logger.info env['REMOTE_ADDR']
+      my_logger.info env['X-Real-IP']
+
+      # puts env
+      #
+      # puts request
+      #
+      # puts  request.env['HTTP_X_FORWARDED_FOR']
+      # puts env["action_dispatch.remote_ip"]
     end
 
 
